014701d53e4df9dc055e0f52be8d3928d19d694d
maxgeorg99 <maxi.georg.mg@gmail.com>
2022-05-15 23:11:51 +0200

    fix path for local ecperiment

--- ide/modules/oracle/real_world_data_source_factory.py
+++ ide/modules/oracle/real_world_data_source_factory.py
@@ -13,9 +13,9 @@ class RealWorldDataSourceFactory():
         return RealWorldDataSetDataSource(q,r)
 
     def office(self):
-        connectivity_file = '/real_world_data_sets/Office/connectivity.txt'
-        data_file = '/real_world_data_sets/Office/data.txt'
-        locs_file = '/real_world_data_sets/Office/mote_locs.txt'
+        connectivity_file = 'C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/Office/connectivity.txt'
+        data_file = 'C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/Office/data.txt'
+        locs_file = 'C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/Office/mote_locs.txt'
 
         df = pd.read_csv(connectivity_file, sep=" ", header=None)
         data_df = pd.read_csv(data_file, sep=" ", header=None)
@@ -24,7 +24,7 @@ class RealWorldDataSourceFactory():
         return RealWorldDataSetDataSource(q,df.to_numpy())
 
     def sunspot(self):
-        filename = '/real_world_data_sets/Sunspot/SN_d_tot_V2.0.txt'
+        filename = 'C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/Sunspot/SN_d_tot_V2.0.txt'
         df = pd.read_csv(filename, sep=" ", header=None)
         q = list(range(1, len(df.index)))
         return RealWorldDataSetDataSource(q,df.to_numpy())
@@ -33,16 +33,16 @@ class RealWorldDataSourceFactory():
         return NotImplemented
     
     def NASDAQ(self):
-        filename = '/real_world_data_sets/NASDAQ/WIKI-PRICES.csv'
+        filename = 'C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/NASDAQ/WIKI-PRICES.csv'
         df = pd.read_csv(filename)
         q = list(range(1, len(df.index)))
         return RealWorldDataSetDataSource(q,df.to_numpy())
     
     def smartphone(self):
-        df = pd.read_csv('/real_world_data_source/Smartphone/final_acc_test.tx', sep=" ", header=None)
-        df2 = pd.read_csv('/real_world_data_source/Smartphone/final_gyro_test.txt', sep=" ", header=None)
-        df3 = pd.read_csv('/real_world_data_source/Smartphone/final_X_test.txt', sep=" ", header=None)
-        df4 = pd.read_csv('/real_world_data_source/Smartphone/final_Y _test.txt', sep=" ", header=None)
+        df = pd.read_csv('C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/Smartphone/final_acc_test.txt', sep=" ", header=None)
+        df2 = pd.read_csv('C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/Smartphone/final_gyro_test.txt', sep=" ", header=None)
+        df3 = pd.read_csv('C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/Smartphone/final_X_test.txt', sep=" ", header=None)
+        df4 = pd.read_csv('C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/Smartphone/final_Y_test.txt', sep=" ", header=None)
         df.merge(df2,on = df.columns.values.tolist(), how = 'right')
         df.merge(df3,on = df.columns.values.tolist(), how = 'right')
         df.merge(df4,on = df.columns.values.tolist(), how = 'right')
@@ -50,9 +50,9 @@ class RealWorldDataSourceFactory():
         return RealWorldDataSetDataSource(q,df.to_numpy())
 
     def hipe(self):
-        filename1 = 'real_world_data_sets/HIPE/PickAndPlaceUnit_PhaseCount_2_geq_2017-10-01_lt_2018-01-01.csv'
-        filename2 = 'real_world_data_sets/HIPE/ScreenPrinter_PhaseCount_2_geq_2017-10-01_lt_2018-01-01.csv'
-        filename3 = 'real_world_data_sets/HIPE/VacuumPump2_PhaseCount_2_geq_2017-10-01_lt_2018-01-01.csv'
+        filename1 = 'C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/HIPE/PickAndPlaceUnit_PhaseCount_2_geq_2017-10-01_lt_2018-01-01.csv'
+        filename2 = 'C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/HIPE/ScreenPrinter_PhaseCount_2_geq_2017-10-01_lt_2018-01-01.csv'
+        filename3 = 'C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/HIPE/VacuumPump2_PhaseCount_2_geq_2017-10-01_lt_2018-01-01.csv'
         df = pd.read_csv(filename1)
         df2 = pd.read_csv(filename2)
         df3 = pd.read_csv(filename3)
@@ -62,10 +62,10 @@ class RealWorldDataSourceFactory():
         return RealWorldDataSetDataSource(q,df.to_numpy())
 
     def hydraulic(self):
-        df = pd.read_csv('/real_world_data_source/HydraulicSystemsDataSet/PS1', sep=" ", header=None)
-        df2 = pd.read_csv('/real_world_data_source/HydraulicSystemsDataSet/PS2', sep=" ", header=None)
-        df3 = pd.read_csv('/real_world_data_source/HydraulicSystemsDataSet/PS3', sep=" ", header=None)
-        df4 = pd.read_csv('/real_world_data_source/HydraulicSystemsDataSet/PS4', sep=" ", header=None)
+        df = pd.read_csv('C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/HydraulicSystemsDataSet/PS1', sep=" ", header=None)
+        df2 = pd.read_csv('C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/HydraulicSystemsDataSet/PS2', sep=" ", header=None)
+        df3 = pd.read_csv('C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/HydraulicSystemsDataSet/PS3', sep=" ", header=None)
+        df4 = pd.read_csv('C:/Users/maxig/ThesisActiveLearningFramework/data_efficient_dependency_estimation/real_world_data_sets/HydraulicSystemsDataSet/PS4', sep=" ", header=None)
         df.merge(df2,on = df.columns.values.tolist(), how = 'right')
         df.merge(df3,on = df.columns.values.tolist(), how = 'right')
         df.merge(df4,on = df.columns.values.tolist(), how = 'right')
@@ -91,4 +91,5 @@ class RealWorldDataSourceFactory():
         return r()
 
     def get_all_data_sources(self):
-        return [self.chf,self.office, self.sunspot, self.personal_activity, self.NASDAQ, self.hipe, self.smartphonem, self.hydraulic]
+        #return [self.chf(),self.office(), self.sunspot(), self.NASDAQ(), self.hipe(), self.smartphone(), self.hydraulic()]
+        return [self.chf(),self.office(), self.NASDAQ(), self.hipe(), self.hydraulic()]
